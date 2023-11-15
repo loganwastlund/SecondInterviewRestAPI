9a14dc16684ad45e5ff67b277b451070c0e4eb62
Logan Wastlund <loganwastlund@gmail.com>
2023-11-15 10:26:24 -0700

    Initial Commit

---
+++ SecondInterviewRestAPI/SecondInterviewRestAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondInterviewRestAPIApp.swift
+//  SecondInterviewRestAPI
+//
+//  Created by Logan on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct SecondInterviewRestAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
